1c1d39f00268c92107e75a0cd923003cff1d3ec0
Leron K. Julian <ljulian@andrew.cmu.edu>
2023-11-11 18:53:37 -0500

    Update warp.py

--- warp.py
+++ warp.py
@@ -44,3 +44,37 @@ def warp(img, UpSampFactor):
         im_warp[:,:,ch] = (1-mask0)*cv2.remap(img[:,:,ch], np.float32(X_res), np.float32(Y_res), cv2.INTER_LINEAR)
       
     return im_warp
+
+
+def unwarp(img, UpSampFactor, rad, cent_x, cent_y, width_to_height=3):
+    rad = rad*1.414
+
+    new_siz = np.ceil((2*rad+1)*UpSampFactor)
+    
+    X0, Y0 = np.meshgrid(np.arange(0, 288), np.arange(0, 352))
+    X0 = (X0 - cent_x)
+    Y0 = (Y0 - cent_y)
+
+    s_new = np.sqrt(X0**2 + Y0**2)
+
+    phi_new = np.angle(X0 + 1j*Y0)
+    mask = (s_new > 0.707*rad)
+
+    s_new[s_new > 0.707*rad] = 0
+
+
+    rho_new = 2*s_new/(2*np.sqrt(rad**2-s_new**2)-rad)
+
+    X_res0 = rho_new*np.cos(phi_new)
+    Y_res0 = rho_new*np.sin(phi_new)
+
+    X_res1 = (1+X_res0/width_to_height)*new_siz/2;
+    Y_res1 = (1+Y_res0/width_to_height)*new_siz/2;
+
+    im_UN_wrap = np.zeros((X_res0.shape[0], X_res0.shape[1], img.shape[2]))
+
+    for ch in range(img.shape[2]):
+        im_UN_wrap[:,:,ch] = (1-mask)*cv2.remap(img[:,:,ch], np.float32(X_res1), np.float32(Y_res1), cv2.INTER_LINEAR)
+    
+    
+    return im_UN_wrap
